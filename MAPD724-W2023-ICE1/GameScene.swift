c3bf913c7b763eb0a9f3e7ae899815bbb4a09070
Kisu <krishna@Krishnas-MacBook-Air.local>
2023-01-18 19:11:07 -0500

    initial commit

---
+++ MAPD724-W2023-ICE1/GameScene.swift
@@ -0,0 +1,50 @@
+//
+//  GameScene.swift
+//  MAPD724-W2023-ICE1
+//
+//  Created by Kisu on 2023-01-18.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+        print("Game Scene Loaded")
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        print("Touch Down")
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        print("Touch Move")
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        print("Touch Up")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
