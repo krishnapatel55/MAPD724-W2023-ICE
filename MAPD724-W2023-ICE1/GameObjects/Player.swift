046c3ede8679769b60439fbe5489e46f9a82c687
Kisu <krishna@Krishnas-MacBook-Air.local>
2023-01-21 23:42:26 -0500

    game object player added

---
+++ MAPD724-W2023-ICE1/GameObjects/Player.swift
@@ -0,0 +1,46 @@
+//
+//  Player.swift
+//  MAPD724-W2023-ICE1
+//
+//  Created by Kisu on 2023-01-21.
+//
+
+import GameplayKit
+import SpriteKit
+
+class Player : GameObject {
+    //constructor / initializer
+    init() {
+        super.init(imageString: "plane", initialScale: 2.0)
+        Start()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //LifeCycle functions
+    override func Start() {
+        zPosition = 2
+        verticleSpeed = 5.0
+    }
+    override func Update() {
+        CheckBounds()
+    }
+    override func CheckBounds() {
+        //constraint the player on the left boundry
+        if (position.x <= -310) {
+            position.x = -310
+        }
+        
+        //constraint the player on the right boundry
+        if (position.x >= 310) {
+            position.x = 310
+        }
+    }
+    override func Reset() {
+        position.y = -495
+    }
+    func TouchMove(newPos: CGPoint) {
+        position = newPos
+    }
+}
