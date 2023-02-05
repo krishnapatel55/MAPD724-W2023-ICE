63e234b9f5f2f954c4b7e8e2a5463c9f84b966e8
Kisu <krishna@Krishnas-MacBook-Air.local>
2023-02-04 22:31:23 -0500

    ScoreManager added

--- MAPD724-W2023-ICE3/GameScene.swift
+++ MAPD724-W2023-ICE3/GameScene.swift
@@ -45,15 +45,22 @@ class GameScene: SKScene {
             addChild(cloud)
         }
         
+        //Engine sound - Background noise/ Music
+        let engineSound = SKAudioNode(fileNamed: "engine.mp3")
+        addChild(engineSound)
+        engineSound.autoplayLooped = true
+        //engineSound.run(SKAction.changeVolume(to: 0.5, duration: 0))
+        
         //preload or prewarm impulse sounds
         do {
             let sounds: [String] = ["thunder", "yay"]
             for sound in sounds
             {
-                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3") ?? "mp3"
+                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3")!
                 let url:URL = URL(fileURLWithPath: path)
                 let avPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
                 avPlayer.prepareToPlay()
+                //avPlayer.setVolume(0.5, fadeDuration: 0)
             }
         }
         catch {
---
+++ MAPD724-W2023-ICE3/Managers/ScoreManager.swift
@@ -0,0 +1,14 @@
+//
+//  ScoreManager.swift
+//  MAPD724-W2023-ICE3
+//
+//  Created by Kisu on 2023-02-04.
+//
+
+import SpriteKit
+import UIKit
+
+class ScoreManager {
+    public static var Score: Int = 0
+    public static var Lives: Int = 5
+}
