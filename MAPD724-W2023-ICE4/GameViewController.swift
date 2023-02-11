da5a03bf3ba563cd4db8da8986521c4752303d46
Kisu <krishna@Krishnas-MacBook-Air.local>
2023-02-11 18:54:09 -0500

    start scene added

--- MAPD724-W2023-ICE4/GameViewController.swift
+++ MAPD724-W2023-ICE4/GameViewController.swift
@@ -11,36 +11,20 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    //Instance Variables
+    var currentScene: GKScene?
+    
     @IBOutlet weak var LivesLabel: UILabel!
     @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet weak var StartLabel: UILabel!
+    @IBOutlet weak var StartButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    view.ignoresSiblingOrder = true
-                }
-            }
-        }
-        
-        //initialize the Lives and Score
+        presentStartScene()
+    
         CollisionManager.gameViewController = self
-        ScoreManager.Score = 0
-        ScoreManager.Lives = 5
-        updateLivesLabel()
-        updateScoreLabel()
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -58,4 +42,48 @@ class GameViewController: UIViewController {
     func updateScoreLabel(){
         ScoreLabel.text = "Score: \(ScoreManager.Score)"
     }
+    
+    func setScene(sceneName: String) -> Void {
+        currentScene = GKScene(fileNamed: sceneName)
+        // Get the SKScene from the loaded GKScene
+        if let scene = currentScene!.rootNode as! SKScene? {
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            if let view = self.view as! SKView? {
+                view.presentScene(scene)
+                view.ignoresSiblingOrder = true
+            }
+        }
+    }
+    
+    func presentStartScene() {
+        ScoreLabel.isHidden = true
+        LivesLabel.isHidden = true
+        StartLabel.isHidden = false
+        StartButton.isHidden = false
+        setScene(sceneName: "StartScene")
+    }
+    
+    func presentEndScene() {
+        
+    }
+    
+    @IBAction func StartButtonPressed(_ sender: UIButton) {
+        ScoreLabel.isHidden = false
+        LivesLabel.isHidden = false
+        StartLabel.isHidden = true
+        StartButton.isHidden = true
+        
+        //initialize the Lives and Score
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        
+        setScene(sceneName: "GameScene")
+    }
+    
 }
